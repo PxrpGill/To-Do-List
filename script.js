650e4e9f37f1f81af823d15abe5b34a1020fd793
Eugene <jhongil3007@gmail.com>
2024-04-06 19:57:53 +0300

    Необходимо доработать стили

--- script.js
+++ script.js
@@ -4,65 +4,89 @@ const list = document.getElementById('list');
 form.addEventListener('submit', function(event) {
     event.preventDefault();
 
+    let deadLine = document.getElementById('deadLine').value;
+    if (deadLine === '') {
+        deadLine = 'Не указано';
+    }
     const text = document.getElementById('textInput').value;
+    document.getElementById('textInput').value = '';
 
-    let listElement = document.createElement('li');
-    listElement.classList.add('listElement');
+    if (text === '') {
+        alert('Нельзя вводить пустое поле!');
+    } else {
+        let listElement = document.createElement('li');
+        listElement.classList.add('listElement');
+        list.appendChild(listElement);
 
-    let forButtonsSpot = document.createElement('div');
-    list.appendChild(forButtonsSpot);
+        const forDeadLineSpot = document.createElement('h3');
+        forDeadLineSpot.textContent = 'Дед лайн: ' + deadLine;
+        listElement.appendChild(forDeadLineSpot);
 
-    let deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Удалить';
-    deleteButton.classList.add('deleteButton');
+        const forTextSpot = document.createElement('p');
+        listElement.appendChild(forTextSpot);
+        const forButtonsSpot = document.createElement('div');
+        listElement.appendChild(forButtonsSpot);
 
-    let editButton = document.createElement('button');
-    editButton.textContent = 'Редактировать';
-    editButton.classList.add('editButton');
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Удалить';
+        deleteButton.classList.add('deleteButton');
 
-    let successPointButton = document.createElement('button');
-    successPointButton.textContent = 'Выполнено';
-    successPointButton.classList.add('successPointButton');
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Редактировать';
+        editButton.classList.add('editButton');
 
-    listElement.textContent = text;
-    list.appendChild(listElement);
-    listElement.appendChild(deleteButton);
-    listElement.appendChild(editButton);
-    listElement.appendChild(successPointButton);
+        const successPointButton = document.createElement('button');
+        successPointButton.textContent = 'Выполнено';
+        successPointButton.classList.add('successPointButton');
 
-    deleteButton.addEventListener('click', function() {
-        list.removeChild(listElement);
-    });
+        forTextSpot.textContent = text;
+        forButtonsSpot.appendChild(deleteButton);
+        forButtonsSpot.appendChild(editButton);
+        forButtonsSpot.appendChild(successPointButton);
 
-    editButton.addEventListener('click', function() {
-        const editForm = document.createElement('form');
-        const editTextSpot = document.createElement('input');
-        const successButton = document.createElement('button');
-        const cancelButton = document.createElement('button');
-        editTextSpot.value = listElement.textContent;
-        successButton.textContent = 'Применить';
-        cancelButton.textContent = 'Отменить';
-        editForm.appendChild(editTextSpot);
-        editForm.appendChild(successButton);
-        editForm.appendChild(cancelButton);
-
-        listElement.appendChild(editForm);
-
-        cancelButton.addEventListener('click', function() {
-            listElement.removeChild(editForm);
+        deleteButton.addEventListener('click', function() {
+            list.removeChild(listElement);
         });
 
-        successButton.addEventListener('click', function() {
-            listElement.textContent = editTextSpot.value;
-            listElement.appendChild(editButton);
-            listElement.appendChild(deleteButton);
-            listElement.appendChild(successPointButton);
-        });
-    });
+        editButton.addEventListener('click', function() {
+            const spotForEditForm = document.createElement('div');
+            const editForm = document.createElement('form');
+            const editTextSpot = document.createElement('input');
+            const successButton = document.createElement('button');
+            successButton.type = 'button';
+            const cancelButton = document.createElement('button');
+            cancelButton.type = 'button';
+            editTextSpot.value = forTextSpot.textContent;
+            successButton.textContent = 'Применить';
+            successButton.classList.add('successPointButton')
+            cancelButton.textContent = 'Отменить';
+            cancelButton.classList.add('deleteButton');
+            editForm.appendChild(editTextSpot);
+            editForm.appendChild(successButton);
+            editForm.appendChild(cancelButton);
 
-    successPointButton.addEventListener('click', function() {
-        listElement.style.color = 'green';
-        listElement.removeChild(editButton);
-        listElement.removeChild(successPointButton);
-    });
+            spotForEditForm.appendChild(editForm);
+            listElement.appendChild(spotForEditForm);
+
+            cancelButton.addEventListener('click', function() {
+                spotForEditForm.removeChild(editForm);
+            });
+
+            successButton.addEventListener('click', function() {
+                if (editTextSpot.value !== '') {
+                    forTextSpot.textContent = editTextSpot.value;
+                    listElement.appendChild(forButtonsSpot);
+                    listElement.removeChild(spotForEditForm);
+                } else {
+                    alert('Вы оставили поле пустым!');
+                }
+            });
+        });
+
+        successPointButton.addEventListener('click', function() {
+            forTextSpot.style.color = 'green';
+            forButtonsSpot.removeChild(editButton);
+            forButtonsSpot.removeChild(successPointButton);
+        });
+    }
 });
